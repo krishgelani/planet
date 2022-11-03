9f8b6776f8efefa8ae4e2af8db13bfac44ee9408
Krish <krishgelani6664@gmail.com>
2022-11-03 21:52:13 +0530

    03-11-2022

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:untitled/HomeScreen.dart';
+
+void main() {
+  runApp(
+    GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    )
+  );
+}
