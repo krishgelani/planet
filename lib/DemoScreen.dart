db9610625f15ce86615ac9125277302353efcd5e
Krish <krishgelani6664@gmail.com>
2023-08-22 22:06:38 +0530

    add demo screen

---
+++ lib/DemoScreen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class DemoScreen extends StatefulWidget {
+  const DemoScreen({super.key});
+
+  @override
+  State<DemoScreen> createState() => _DemoScreenState();
+}
+
+class _DemoScreenState extends State<DemoScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Demo"),
+      ),
+      body: const Center(
+        child: Text("Demo"),
+      ),
+    );
+  }
+}
